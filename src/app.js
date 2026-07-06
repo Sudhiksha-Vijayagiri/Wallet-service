a6be36343f9dffb25e1184458f8b4252fcbfda11
Sudhiksha Vijayagiri <hansikavijayagiri@gmail.com>
2026-07-07 03:16:41 +0530

    Added Frontend

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
 const swaggerUi = require('swagger-ui-express');
 const swaggerSpec = require('./config/swagger');
 const routes = require('./routes');
@@ -9,6 +10,7 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(express.static(path.join(__dirname, '..', 'public')));
 
 // simple health check, useful for docker/uptime checks later
 app.get('/health', (req, res) => {
